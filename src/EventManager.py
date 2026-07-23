0415e36d8d2e09f03cbdcc5e421afa0eaee91db2
Ethan <ethanalmonte781@gmail.com>
2026-07-22 20:34:04 -0400

    Improved event queue system

--- src/EventManager.py
+++ src/EventManager.py
@@ -11,25 +11,34 @@ logger = logging.getLogger(__name__)
 class EventManager:
 
     def __init__(self):
-
         self.events = queue.PriorityQueue()
+
+        logger.info(
+            "Event manager initialized"
+        )
 
 
     def add_event(
         self,
-        priority,
-        event_type,
-        data
+        priority: int,
+        event_type: str,
+        data: dict
     ):
+
+        event = {
+            "type": event_type,
+            "data": data
+        }
 
         self.events.put(
             (
                 -priority,
-                {
-                    "type": event_type,
-                    "data": data
-                }
+                event
             )
+        )
+
+        logger.info(
+            f"Event added: {event_type}"
         )
 
 
@@ -39,3 +48,8 @@ class EventManager:
             return None
 
         return self.events.get()[1]
+
+
+    def has_events(self):
+
+        return not self.events.empty()
